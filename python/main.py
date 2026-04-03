bcfad1be8c88bdabbf5f2e6d7fe41f69bb2e30f7
K <Akarfire@gmail.com>
2026-04-03 12:39:29 +0300

    [DEV] Python example fix

--- python/main.py
+++ python/main.py
@@ -1,11 +1,9 @@
 import splinesolver
 
-print (splinesolver.solve_spline(range(0, 10), range(0, 10)))
-
 # Пример использования сплайна
 x = [0.0, 1.0, 2.0]
 y = [0.0, 1.0, 4.0]
 
-result = sl.solve_spline(x, y)
+result = splinesolver.solve_spline(x, y)
 for i, (a, b, c, d) in enumerate(result):
     print(f"Segment {i}: a={a}, b={b}, c={c}, d={d}")
