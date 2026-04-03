fadc7f11c8518ef585e04a625d99b7332b58a5c2
unknown <the_h1nk@mail.ru>
2026-04-03 12:22:01 +0300

    [DEV] add wrapper for python and cpp compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include "solver.h"
 #include "pybind11/pybind11.h"
-
-int return_0()
-{
-    return 0;
-}
+#include "pybind11/stl.h"
 
 // Create the pybind11 module
 PYBIND11_MODULE(splinesolver, m) {
     m.doc() = "Solves cubic splines";
     
-    // Wrap the return_0 function
-    m.def("return_0", &return_0, "A function that returns 0");
+    m.def("solve_spline", &Solver::SolveSpline,
+        pybind11::arg("x_list"), pybind11::arg("y_list")    
+    );
 }
 
 int main() {
